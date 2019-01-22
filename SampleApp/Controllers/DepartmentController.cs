f117dc34892f3977ac82c5024b4fb21a5d811451
hipreetham <hipreetham@gmail.com>
2019-01-22 06:05:49 -0600

    Added a Department Controller and a view, connect the view to the controller.

---
+++ SampleApp/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Employees()
+        {
+            return View("Colleagues");
+        }
+
+    }
+}
